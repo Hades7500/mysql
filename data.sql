69dd1d24e64d67fd4c8a3868478aa47b53412b61
Maddy <deadly16maddy@gmail.com>
2023-05-28 16:20:52 +0530

    Added India

--- data.sql
+++ data.sql
@@ -1,2 +1,1 @@
-INSERT INTO `Teams` VALUES ("ENG","England"),
-("AUS","Australia"),("BAN","Bangladesh"),("AFG","Afghanistan"),("WI","West Indies"),("SL","Sri Lanka"),("Pak","Pakistan");
+INSERT INTO `Teams` VALUES ("ENG","England"),("IND","India"),("AUS","Australia"),("BAN","Bangladesh"),("AFG","Afghanistan"),("WI","West Indies"),("SL","Sri Lanka"),("PAK","Pakistan");
