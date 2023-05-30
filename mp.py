90b64eb7c4a06cc89047e19bc3784c25f1d14cf6
Abhinav Gupta <hades7963@protonmail.com>
2023-05-30 22:59:45 +0530

    Modified Database Structure

--- mp.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-label=['IND','NZ','SA','ENG','AUS','WI','SL','BAN','AFG','PAK']
-per=[10,1200,50,16,13,25,19,21,19,19]
-bar = plt.bar(label, per)
-bar[1].set_color("Yellow")
-plt.xlabel('Teams',fontsize=20)
-plt.ylabel('Runs',fontsize=20)
-plt.title('Total Runs scored by each team')
-plt.show()
