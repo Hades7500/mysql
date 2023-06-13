fe09d05063afe3c3e3bdd4c07746d2a7f9d59fcb
Abhinav Gupta <hades7963@protonmail.com>
2023-06-13 13:49:14 +0530

    Added requirements.txt

--- gui.py
+++ gui.py
@@ -8,15 +8,13 @@ window.geometry("1600x900")
 
 ttk.Label(window, text = "World Cup 2023", font = "SaxMono 50").pack()
 
-output_frame = ttk.Frame(window)
-output_frame.place(x = 100, y = 100)
-
 style = ttk.Style()
 
-
+# Notebook
 notebook = ttk.Notebook(window)
 
 tab1 = ttk.Frame(notebook)
+tab1.configure()
 tab2 = ttk.Frame(notebook)
 notebook.add(tab1, text = "Teams")
 notebook.add(tab2, text = "Players")
@@ -24,13 +22,11 @@ notebook.add(tab2, text = "Players")
 notebook.pack()
 
 def display_teams():
-    ...
-
     teams = Queries.display_teams()
-    style.configure("Treeview", font = ("SaxMono", 15))
-    style.configure("Treeview.Headings", font = ("SaxMono", 30))
-    table = ttk.Treeview(tab1, columns = ("TeamID", "TeamName"), show = "headings", style = "Treeview")
-    table.column("TeamID", width = 100)
+    style.configure("m.Treeview", font = ("SaxMono", 15))
+    style.configure("m.Treeview.Heading", font = ("SaxMono", 20))
+    table = ttk.Treeview(tab1, columns = ("TeamID", "TeamName"), show = "headings", style = "m.Treeview")
+    table.column("TeamID", width = 90)
     table.heading("TeamID", text = "TeamID")
     table.heading("TeamName", text = "Team Name")
     for i in teams:
@@ -38,11 +34,13 @@ def display_teams():
     
     table.pack(fill = "both", expand = True)
 
-view_teams = ttk.Button(window, text = "View Teams", command = display_teams)
-view_teams.pack()
+def add_team():
+    ...
 
-#Bindings
-output_frame.bind_class('Entry', '<FocusIn>', lambda event: print("works"))
+add_team = ttk.Button(window, text = "View Teams", command = add_team)
+add_team.pack()
+
+# Bindings
 
 # run 
 window.mainloop()
